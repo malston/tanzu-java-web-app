cede5de3aa0711e346977a6ac7fcbf35a2eb564e
Mark Alston <malston@vmware.com>
2022-06-23 16:58:24 -0600

    Update greeting

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Mark + Spring Boot + Tanzu!";
+		return "Greetings from Marksalot + Spring Boot + Tanzu!";
 	}
 
 }
